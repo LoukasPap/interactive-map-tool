47898f52678c1440b90a2abec769f99095606810
LoukasPap <loukas.ppd@gmail.com>
2025-08-14 01:02:11 +0300

    Fix bug where QuickSelectionButtons triggered accordion state

--- ascsa-map-tool/src/Components/FilterBar/QuickSelectionButtons.jsx
+++ ascsa-map-tool/src/Components/FilterBar/QuickSelectionButtons.jsx
@@ -1,11 +1,12 @@
 import { Stack, IconButton } from "@chakra-ui/react";
 import { LuSquareCheck, LuSquareMinus } from "react-icons/lu";
 
-const QuickSelectionButtons = ({handleSelectAll, handleClearAll}) => {
+const QuickSelectionButtons = ({ handleSelectAll, handleClearAll }) => {
   return (
     <Stack
       direction={{ lg: "row", md: "column" }}
       justifyContent="space-around"
+      gap={0}
     >
       <IconButton
         size="2xl"
@@ -13,9 +14,15 @@ const QuickSelectionButtons = ({handleSelectAll, handleClearAll}) => {
         h="fit"
         variant="plain"
         // Disable event bubbling to not trigger accordion expansion/contraction
-        onClick={(e) => {e.stopPropagation(); handleSelectAll();}} 
+        onClick={(e) => {
+          e.stopPropagation();
+          handleSelectAll();
+        }}
         _hover={{ bg: "gray.300" }}
         p={2}
+        fontSize="md"
+        gap={1}
+
       >
         <LuSquareCheck />
         Select all
@@ -27,9 +34,14 @@ const QuickSelectionButtons = ({handleSelectAll, handleClearAll}) => {
         w="fit"
         h="fit"
         variant="plain"
-        onClick={(e) => {e.stopPropagation(); handleClearAll()}}
+        onClick={(e) => {
+          e.stopPropagation();
+          handleClearAll();
+        }}
         _hover={{ bg: "gray.300" }}
         p={2}
+        fontSize="md"
+        gap={1}
       >
         <LuSquareMinus size={"xl"} /> Clear
       </IconButton>
