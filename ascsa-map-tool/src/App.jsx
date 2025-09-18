60f8eeb4768956316723fcd6b7fa74b50c3b009e
LoukasPap <loukas.ppd@gmail.com>
2025-09-18 19:42:37 +0300

    Fix Leaflet's default icon paths

--- ascsa-map-tool/src/App.jsx
+++ ascsa-map-tool/src/App.jsx
@@ -1,5 +1,18 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet's default icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 import MapLayer from "./Components/MapLayer";
 
 function App() {
