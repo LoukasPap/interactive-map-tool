c5aaa96f80e589ccc6ce5eb78f879b02ceeb80ab
LoukasPap <loukas.ppd@gmail.com>
2025-10-22 01:48:54 +0300

    Add ShapeHelpers

---
+++ ascsa-map-tool/src/Components/ShapeHelpers.js
@@ -0,0 +1,72 @@
+export function getShapeProperties(shapeType, obj) {
+  const layer = obj.layer || obj;
+  console.log("layer", layer);
+
+  let props = {
+    type: shapeType,
+    geojson: layer.toGeoJSON(),
+    options: { pane: "overlayPane" },
+  };
+
+  if (shapeType == "Circle") {
+    props.options.radius = layer.getRadius();
+  }
+
+  return props;
+}
+
+export function restoreShape(map, shapeData) {
+  if (!shapeData || !shapeData.type) return null;
+
+  const { type, geojson, options } = shapeData;
+
+  let layer = null;
+
+  switch (type) {
+    case "Circle": {
+      const [lng, lat] = geojson.geometry.coordinates;
+      const radius = options?.radius ?? 0;
+      layer = L.circle([lat, lng], { ...options, radius });
+      break;
+    }
+
+    case "Rectangle": {
+      layer = L.rectangle(
+        geojson.geometry.coordinates[0].map(([lng, lat]) => [lat, lng]),
+        options
+      );
+      break;
+    }
+
+    case "Polygon": {
+      layer = L.polygon(
+        geojson.geometry.coordinates[0].map(([lng, lat]) => [lat, lng]),
+        options
+      );
+      break;
+    }
+
+    default:
+      console.warn("Unknown shape type:", type);
+  }
+
+  if (layer) {
+    if (layer.pm) {
+      layer.pm.enable({
+        allowEditing: true,
+        draggable: true,
+        snappable: false,
+      });
+    }
+  }
+
+  return layer;
+}
+
+export function removeShapeLayer(c) {
+  if (c.shape.layer) {
+    c.shape.layer.remove();
+  } else {
+    c.shape.remove();
+  }
+}
